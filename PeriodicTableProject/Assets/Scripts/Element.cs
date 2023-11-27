281d0a740a8b8890346ad33e4351bb575ca39ca0
Valentin Chivliev <valentin.chivliev02@gmail.com>
2023-11-27 13:18:59 +0200

    Restarting of the project

---
+++ PeriodicTableProject/Assets/Scripts/Element.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour 
+{
+	public string elementName;
+	public string elementSymbol;
+	public string description;
+	public int atomicNumber;
+	public double atomicWeight;
+	public double meltingPoint;
+	public double boilingPoint;
+	public string discoveredBy;
+	public GameObject bohrModelPrefab;
+	public Canvas elementCanvas;
+
+	private Vector3 originalPosition;
+	private bool isOpen = false;
+	
+	private void Start()
+    {
+		// Store the original position of the element
+		originalPosition = transform.position;
+	}
+	private void OnMouseDown()
+    {
+        if (!isOpen)
+        {
+			OpenElementBox();
+        }
+        else
+        {
+			CloseElementBox();
+			float delay = 3.0f;
+			Invoke("ReturnToOriginalPosition",delay);
+		}
+    }
+
+    private void OpenElementBox()
+    {
+		// Check if elementCanvas is assigned
+		if (elementCanvas == null)
+		{
+			Debug.LogError("Element Canvas is not assigned. Please assign it in the Inspector.");
+			return;
+		}
+		ElementInfoUI elementInfoUI = elementCanvas.GetComponent<ElementInfoUI>();
+		if (elementInfoUI == null)
+		{
+			Debug.LogError("ElementInfoUI script is not found on the Element Canvas. Please attach it.");
+			return;
+		}
+		// Move the element closer to the player (you can adjust the position)
+		Vector3 targetPosition = Camera.main.transform.position + Camera.main.transform.forward * 5f;
+		float speed = 2f;
+		while (Vector3.Distance(transform.position, targetPosition) > 0.1f)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+		}
+
+		// Show  information on the UI canvas
+		elementCanvas.gameObject.SetActive(true);
+		elementCanvas.GetComponent<ElementInfoUI>().UpdateInfo(elementName, elementSymbol, description, atomicNumber, atomicWeight,
+			meltingPoint, boilingPoint, discoveredBy);
+
+		// Show 3D Bohr model
+		Instantiate(bohrModelPrefab, transform.position + new Vector3(1f, 0f, 0f), Quaternion.identity);
+
+		isOpen = true;
+	}
+
+	private void CloseElementBox()
+    {
+		// Move the element back to its original position
+		Vector3 targetPosition = originalPosition;
+		float speed = 2f;
+
+		// Hide the UI canvas
+		elementCanvas.gameObject.SetActive(false);
+
+		// Destroy the 3D Bohr model
+		Destroy(GameObject.Find(bohrModelPrefab.name + "(Clone)"));
+
+		isOpen = false;
+	}
+}
---
+++ PeriodicTableProject/Assets/Scripts/ElementInfoUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElementInfoUI : MonoBehaviour 
+{
+	public Text elementNameText;
+	public Text elementSymbolText;
+	public Text descriptionText;
+	public Text atomicNumberText;
+	public Text atomicWeightText;
+	public Text meltingPointText;
+	public Text boilingPointText;
+	public Text discoveredByText;
+
+	public void UpdateInfo(string elementName, string elementSymbol, string description, int atomicNumber, double atomicWeight,
+		double meltingPoint, double boilingPoint, string discoveredBy)
+    {
+		elementNameText.text = elementName;
+		elementSymbolText.text = elementSymbol;
+		descriptionText.text = description;
+		atomicNumberText.text = "Atomic Number:" + atomicNumber;
+		atomicWeightText.text = "Atomic Weight: " + atomicWeight;
+		meltingPointText.text = "Melting Poing: " + meltingPoint;
+		boilingPointText.text = "Boiling Point: " + boilingPoint;
+		discoveredByText.text = "Discovered By: " + discoveredBy;
+
+	}
+}
