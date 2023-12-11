d5dda725bb1e6065f87041ffb97984e49d2389d2
Valentin Chivliev <valentin.chivliev02@gmail.com>
2023-12-11 02:13:54 +0200

    PeriodicTable 1.0.0

--- PeriodicTableProject/Assets/Scripts/Element.cs
+++ PeriodicTableProject/Assets/Scripts/Element.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Element : MonoBehaviour 
 {
@@ -14,11 +15,14 @@ public class Element : MonoBehaviour
 	public double boilingPoint;
 	public string discoveredBy;
 	public int yearOfDiscovery;
+
 	public GameObject bohrModelPrefab;
+
 	public Canvas elementCanvas;
 
 	private Vector3 originalPosition;
 	private Vector3 originalScale;
+
 	public bool isOpen = false;
 	
 	private void Start()
@@ -28,6 +32,7 @@ public class Element : MonoBehaviour
 		originalPosition = transform.position;
 		originalScale = transform.localScale;
 	}
+
 	private void OnMouseDown()
     {
 		if (isOpen == false)
@@ -39,13 +44,19 @@ public class Element : MonoBehaviour
 			CloseElementBox();
 		}
 	}
+
     private void OpenElementBox()
     {
-		// Move the element closer to the player (you can adjust the position)
-		Vector3 targetPosition = Camera.main.transform.position + Camera.main.transform.forward * 5f;
+		// Move the element closer to the player and scale it (adjust the position if needed)
+		Vector3 targetPosition = Camera.main.transform.position + Camera.main.transform.forward * 1f;
+		targetPosition.y = -0.4f;
+		float scaleFactor = 0.2f;
+
 		float speed = 2f;
+
 		while (Vector3.Distance(transform.position, targetPosition) > 0.1f)
 		{
+			transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
 			transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
 		}
 
@@ -60,26 +71,69 @@ public class Element : MonoBehaviour
 		Mathf.Floor(transform.position.y),
 		Mathf.Floor(transform.position.z)
 		);
-		Instantiate(bohrModelPrefab, roundedPosition + new Vector3(0f, 0f, -4.25f), Quaternion.identity).
+        if (bohrModelPrefab != null)
+        {
+			Instantiate(bohrModelPrefab, roundedPosition + new Vector3(0f, 1f, -0.25f), Quaternion.identity).
 			transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+			bohrModelPrefab.GetComponent<Animator>();
+		}
+        else
+        {
+			Debug.LogError("Bohr Model Prefab is not assigned.");
+		}
+		
+		Collider[] allColliders = FindObjectsOfType<Collider>();
+
+		// Disable each collider
+		foreach (Collider collider in allColliders)
+		{
+			// Local scale of the collider's GameObject
+			Vector3 colliderScale = collider.transform.localScale;
+
+			// Check the y component of the scale
+			if (colliderScale.y > 0.9f)
+			{
+				collider.enabled = false;
+			}
+			else
+			{
+				collider.enabled = true;
+			}
+		}
+
 		isOpen = true;
 	}
 
 	public void CloseElementBox()
     {
-		// Move the element back to its original position
+		// Move the element back to its original position and scale it to the original size
 		Vector3 targetPosition = originalPosition;
+		Vector3 targetScale = originalScale;
 		float speed = 2f;
 
 		while (Vector3.Distance(transform.position, targetPosition) > 0.0f)
 		{
+			transform.localScale = targetScale;
 			transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
 		}
+
 		// Hide the UI canvas
 		elementCanvas.gameObject.SetActive(false);
 
-		// Destroy the 3D Bohr model
-		Destroy(GameObject.Find(bohrModelPrefab.name + "(Clone)"));
+        // Destroy the 3D Bohr model
+        if (bohrModelPrefab != null)
+        {
+			Destroy(GameObject.Find(bohrModelPrefab.name + "(Clone)"));
+		}
+
+		Collider[] allColliders = FindObjectsOfType<Collider>();
+
+		// Enable each collider
+		foreach (Collider collider in allColliders)
+		{
+			collider.enabled = true;
+		}
 
 		isOpen = false;
 	}
--- PeriodicTableProject/Assets/Scripts/ElementInfoUI.cs
+++ PeriodicTableProject/Assets/Scripts/ElementInfoUI.cs
@@ -18,15 +18,63 @@ public class ElementInfoUI : MonoBehaviour
 	public void UpdateInfo(string elementName, string elementSymbol, string description, int atomicNumber, double atomicWeight,
 		double meltingPoint, double boilingPoint, string discoveredBy, int yearOfDiscovery)
     {
+		// elementNameText
 		elementNameText.text = elementName;
-		elementSymbolText.text = elementSymbol;
-		descriptionText.text = description;
-		atomicNumberText.text = "Атомен номер: " + atomicNumber ;
-		atomicWeightText.text = "Атомна маса: " + atomicWeight + " u";
-		meltingPointText.text = "Температура на топене: " + meltingPoint + " °C";
-		boilingPointText.text = "Температура на кипене: " + boilingPoint + " °C";
-		discoveredByText.text = "Открит от: " + discoveredBy;
-		yearOfDiscoveryText.text = "Година на откритие: " + yearOfDiscovery;
 
+		// elementSymbolText
+		elementSymbolText.text = elementSymbol;
+
+		// descriptionText
+		descriptionText.text = description;
+
+		// atomicNumberText
+		atomicNumberText.text = "Атомен номер: " + atomicNumber ;
+
+		// atomicWeightText
+		atomicWeightText.text = "Атомна маса: " + atomicWeight + " u";
+
+		// meltingPointText
+		if (meltingPoint != 0)
+		{
+			meltingPointText.text = "Температура на топене: " + meltingPoint + " °C";
+			meltingPointText.gameObject.SetActive(true);
+		}
+		else
+		{
+			meltingPointText.gameObject.SetActive(false);
+		}
+
+		// boilingPointText
+		if (boilingPoint != 0)
+		{
+			boilingPointText.text = "Температура на кипене: " + boilingPoint + " °C";
+			boilingPointText.gameObject.SetActive(true);
+		}
+		else
+		{
+			boilingPointText.gameObject.SetActive(false);
+		}
+
+		// discoveredByText
+		if (!string.IsNullOrEmpty(discoveredBy))
+        {
+			discoveredByText.text = "Открит от: " + discoveredBy;
+			discoveredByText.gameObject.SetActive(true);
+		}
+        else
+        {
+			discoveredByText.gameObject.SetActive(false);
+		}
+
+		// yearOfDiscoveryText
+		if (yearOfDiscovery != 0)
+        {
+			yearOfDiscoveryText.text = "Година на откритие: " + yearOfDiscovery;
+			yearOfDiscoveryText.gameObject.SetActive(true);
+		}
+		else
+		{
+			yearOfDiscoveryText.gameObject.SetActive(false);
+		}
 	}
 }
